2066242cd72725813283dd4bb2d40b37a0a37a6b
zzzzuny <zzzzuny@gmail.com>
2025-08-18 23:54:44 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  basePath: isProd ? '/gw-jh_wedding_invitation' : '',
+  assetPrefix: isProd ? '/gw-jh_wedding_invitation/' : '',
 };
 
 export default nextConfig;
